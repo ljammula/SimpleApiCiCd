a9b9a781424a2360c8642359414e170ed4a5f316
jlnarsimhareddy@gmail.com <jlnarsimhareddy@gmail.com>
2020-10-28 00:12:52 -0600

    remove logger constructor

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -16,15 +16,9 @@ namespace SimpleAPI.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
+        
         public WeatherForecastController()
-        { 
+        {
             //nothing here
         }
 
